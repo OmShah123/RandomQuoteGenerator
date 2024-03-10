8bcf1f8c12fe8bf8254c82e20fb219a61b036520
Om Shah <omdshah2004@gmail.com>
2024-03-10 05:18:36 -0500

    Initial Commit

---
+++ RandomQuoteGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomQuoteGenerator
+//
+//  Created by Shah, Om on 3/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomQuoteGenerator/RandomQuoteGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteGeneratorApp.swift
+//  RandomQuoteGenerator
+//
+//  Created by Shah, Om on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
